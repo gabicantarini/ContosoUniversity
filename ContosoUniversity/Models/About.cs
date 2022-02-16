53000f8972a4bc6a2e5837ad811af2025e541e4d
gabicantarini <gabilana64@hotmail.com>
2022-02-16 23:26:50 +0000

    feat: about page test

---
+++ ContosoUniversity/Models/About.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class About
+    {
+       
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
